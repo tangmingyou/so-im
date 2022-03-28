23916913d304cc4544e109799af58f05694da49e
tangmingyou <234767776@qq.com>
2022-03-29 00:29:46 +0800

    dubbo spring

---
+++ im-entry/src/main/java/net/sopod/soim/entry/EntryApplication.java
@@ -0,0 +1,23 @@
+package net.sopod.soim.entry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * EntryMain
+ *
+ * @author tmy
+ * @date 2022-03-26 00:06
+ */
+@SpringBootApplication
+public class EntryApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(EntryApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntryApplication.class, args);
+    }
+
+}
---
+++ im-entry/src/main/java/net/sopod/soim/entry/server/EntryServerStarter.java
@@ -0,0 +1,29 @@
+package net.sopod.soim.entry.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * ServerRunner
+ *
+ * @author tmy
+ * @date 2022-03-29 00:22
+ */
+@Component
+public class EntryServerStarter implements ApplicationRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(EntryServerStarter.class);
+
+    @Override
+    public void run(ApplicationArguments args) {
+        EntryServer entryServer = new EntryServer("entry server", 8088);
+        entryServer.startServer(err -> {
+            logger.error("EntryServer 启动失败:", err);
+        });
+        Runtime.getRuntime().addShutdownHook(new Thread(entryServer::shutdown));
+    }
+
+}
