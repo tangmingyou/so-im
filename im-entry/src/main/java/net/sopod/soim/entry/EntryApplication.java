bec5c36cab014053985944e30ebfd346e0033a7d
tangmingyou <234767776@qq.com>
2022-03-31 09:31:37 +0800

    log4j2

--- im-entry/src/main/java/net/sopod/soim/entry/EntryApplication.java
+++ im-entry/src/main/java/net/sopod/soim/entry/EntryApplication.java
@@ -14,7 +14,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EntryApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(EntryApplication.class);
+
     public static void main(String[] args) {
+        logger.info("二狗腿子:{}", 1);
+        logger.info("二狗腿子:{}", 2);
+        logger.info("二狗腿子:{}", 3);
+        System.out.println("狗腿子4");
         SpringApplication.run(EntryApplication.class, args);
     }
 
