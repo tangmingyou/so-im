3a66437210ccf5ab0a4ca2155fbd6fc0e64a4245
tangmingyou <234767776@qq.com>
2022-05-18 23:24:36 +0800

    调用方 protobuf 和 hassion不兼容

--- im-entry/src/main/java/net/sopod/soim/entry/handlers/friend/ReqFriendSearchHandler.java
+++ im-entry/src/main/java/net/sopod/soim/entry/handlers/friend/ReqFriendSearchHandler.java
@@ -5,10 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import net.sopod.soim.data.msg.friend.Friend;
 import net.sopod.soim.entry.server.handler.AccountMessageHandler;
 import net.sopod.soim.entry.server.session.Account;
-import net.sopod.soim.logic.api.friend.search.AccountInfo;
-import net.sopod.soim.logic.api.friend.search.AccountSearchService;
-import net.sopod.soim.logic.api.friend.search.ReqAccountSearch;
-import net.sopod.soim.logic.api.friend.search.ResAccountSearch;
+//import net.sopod.soim.logic.api.friend.search.AccountInfo;
+//import net.sopod.soim.logic.api.friend.search.AccountSearchService;
+//import net.sopod.soim.logic.api.friend.search.ReqAccountSearch;
+//import net.sopod.soim.logic.api.friend.search.ResAccountSearch;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.stereotype.Service;
 
@@ -27,28 +27,33 @@ import java.util.stream.Collectors;
 @Service
 public class ReqFriendSearchHandler extends AccountMessageHandler<Friend.ReqFriendSearch> {
 
-    @DubboReference
-    private AccountSearchService accountSearchService;
-
     @Override
     public MessageLite handle(Account account, Friend.ReqFriendSearch msg) {
-        msg.getKeyword();
-        ReqAccountSearch search = ReqAccountSearch.newBuilder().setKeyword(msg.getKeyword()).build();
-        CompletableFuture<ResAccountSearch> future = accountSearchService.searchAccountLikelyAsync(search);
-        future.thenAccept(res -> {
-            log.info("用户搜索开始...");
-            List<AccountInfo> accounts = res.getAccountsList();
-            List<Friend.UserInfo> userInfos = accounts.stream().map(
-                    acc -> Friend.UserInfo.newBuilder()
-                            .setAccount(acc.getAccount())
-                            .setNickname(acc.getNickname())
-                            .setUid(acc.getUid())
-                            .build()
-            ).collect(Collectors.toList());
-            Friend.ResFriendSearch resSearch = Friend.ResFriendSearch.newBuilder().addAllUsers(userInfos).build();
-            account.writeNow(resSearch);
-        });
         return null;
     }
+
+//    @DubboReference
+//    private AccountSearchService accountSearchService;
+//
+//    @Override
+//    public MessageLite handle(Account account, Friend.ReqFriendSearch msg) {
+//        msg.getKeyword();
+//        ReqAccountSearch search = ReqAccountSearch.newBuilder().setKeyword(msg.getKeyword()).build();
+//        CompletableFuture<ResAccountSearch> future = accountSearchService.searchAccountLikelyAsync(search);
+//        future.thenAccept(res -> {
+//            log.info("用户搜索开始...");
+//            List<AccountInfo> accounts = res.getAccountsList();
+//            List<Friend.UserInfo> userInfos = accounts.stream().map(
+//                    acc -> Friend.UserInfo.newBuilder()
+//                            .setAccount(acc.getAccount())
+//                            .setNickname(acc.getNickname())
+//                            .setUid(acc.getUid())
+//                            .build()
+//            ).collect(Collectors.toList());
+//            Friend.ResFriendSearch resSearch = Friend.ResFriendSearch.newBuilder().addAllUsers(userInfos).build();
+//            account.writeNow(resSearch);
+//        });
+//        return null;
+//    }
 
 }
